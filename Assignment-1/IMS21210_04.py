3859d8eb7500b3a2107b484b0b54064b2d878230
Praharsha <chimalapraharsha21@iisertvm.ac.in>
2024-02-09 21:49:14 +0530

    Assignment-1, question 04

---
+++ Assignment-1/IMS21210_04.py
@@ -0,0 +1,10 @@
+# Input: Get three integers from the user
+first_integer = int(input("Input the first integer: "))
+second_integer = int(input("Input the second integer: "))
+third_integer = int(input("Input the third integer: "))
+
+# Find the maximum value among the three integers
+maximum_value = max(first_integer, second_integer, third_integer)
+
+# Output the result
+print(f"Maximum value of three integers: {maximum_value}")
