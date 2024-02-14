d96f75da051ce5048eae8868c0a745608a167058
Praharsha <chimalapraharsha21@iisertvm.ac.in>
2024-02-14 10:15:46 +0530

    appropriate comments are added.

--- Jacobi_iterative_method.py
+++ Jacobi_iterative_method.py
@@ -1,34 +1,32 @@
 import numpy.linalg as alg
 import numpy as np
 
+
+# Jacobi fixed point iterative method to solve system of linear equations
+def jacobi_iterative(A_matrix, b_matrix, max_iter: int = 100, tol: float = 0.0001):
+    x_guess = np.zeros(len(A_matrix))  # random guess initialization
+    x = np.zeros(len(A_matrix))  # copy of x_guess to store the next i+1 guess by update
+    for iteration in range(max_iter):  # loop through maximum iterations
+        for i in range(len(A_matrix)):  # i = 0, 1, 2,...n- dimension of system of linear equation
+            dum_sum = 0  # initialize summation
+            for j in range(len(A_matrix)):  # j = 0, 1, 2,...n- dimension of system of linear equation
+                if j != i:  # taking non-diagonal elements
+                    dum_sum += A_matrix[i][j] * x_guess[j]  # summation term
+            x[i] = (- dum_sum + b_matrix[i]) / A_matrix[i][i]  # calculating next guess
+        if abs(alg.norm(x - x_guess)) < tol:  # checking if relative error is exceeding tolerance value
+            print(f"The solution at {iteration} iteration\nx: {x}")
+            raise Warning('The procedure is successful')
+    # maximum iterations reached and returns solution as x matrix
+    return print(f"Maximum iterations reached!\n The solution at {iteration} iteration\nx: {x}")
+
+
+# matrix of system of linear equations
 A = np.array([[10, -1, 2, 0.],
               [-1, 11, -1, 3],
               [2, -1, 10, -1],
               [0., 3, -1, 8]])
 
+# constants
 b = np.array([6, 25, -11, 15])
-
-n = len(A)
-
-
-# print(x0)
-
-def jacobi_iterative(A_matrix, b_matrix, max_iter: int = 100, tol: float = 0.0001):
-
-    x0 = np.zeros(len(A_matrix))
-    x = np.zeros(len(A_matrix))
-    for iteration in range(max_iter):
-        for i in range(len(A_matrix)):
-            dum_sum = 0
-            for j in range(len(A_matrix)):
-                if j != i:
-                    dum_sum += A_matrix[i][j] * x0[j]
-            x[i] = (- dum_sum + b_matrix[i]) / A_matrix[i][i]
-        if abs(alg.norm(x - x0)) < tol:
-            print(f"The solution at {iteration} iteration\nx: {x}")
-            raise Warning('The procedure is successful')
-
-    return print(f"Maximum iterations reached!\n The solution at {iteration} iteration\nx: {x}")
-
 
 jacobi_iterative(A, b, max_iter=1000)
