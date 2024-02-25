602af1a503317ed710e7f1855722a95cc31b763f
Praharsha <chimalapraharsha21@iisertvm.ac.in>
2024-02-25 13:54:59 +0530

    fixed iterative method updated.

--- 3.Fixed_point_iteration_method.py
+++ 3.Fixed_point_iteration_method.py
@@ -1,14 +1,15 @@
-def fixed_point(func, p0, max_iter, tol):
+def fixed_point_iterative(func, p_n, max_iter=1000, tol=1.0e-5):
     for iteration in range(max_iter):
-        p = func(p0)
-        if abs(p - p0) < tol:
+        p = func(p_n)
+        if abs(p - p_n) < tol:
             return p
         else:
-            p0 = p
-    # print(p)
-    return p
-def function(x):
-    return x**2 - 2
+            p_n = p
 
-sol = fixed_point(function, 1.5, 10000, 0.0001)
-print(sol)
+
+def function(x):
+    return (x ** 2 - 1) / 2
+
+
+soln = fixed_point_iterative(function, 1.5)
+print(soln)
