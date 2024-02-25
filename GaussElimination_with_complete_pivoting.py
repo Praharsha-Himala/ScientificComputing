6ceade80b7f8fead0bbe7dfcf3c6a26e92ff5637
Praharsha <chimalapraharsha21@iisertvm.ac.in>
2024-02-25 22:48:34 +0530

    updating.

--- GaussElimination_with_complete_pivoting.py
+++ GaussElimination_with_complete_pivoting.py
@@ -10,13 +10,15 @@ def GaussElimination_complete_pivoting(A_matrix, b_matrix):
         A[[k, max_row_index + k]] = A[[max_row_index + k, k]]
         b[[k, max_row_index + k]] = b[[max_row_index + k, k]]
         A[:, [k, max_col_index + k]] = A[:, [max_col_index + k, k]]
-    #
     # print(A)
     # print(b)
     # print(max_row_index, max_col_index)
     for k in range(0, n - 1):
         for i in range(k + 1, n):
-            if A_matrix[]
+            if A_matrix[k, k] == 0:
+                    if A_matrix[i, k] != 0:
+                        A_matrix[[k, i]] = A_matrix[[i, k]]
+                        b_matrix[[k, i]] = b_matrix[[i, k]]
             if A_matrix[i, k] == 0:
                 pass
             else:
@@ -42,4 +44,4 @@ A = np.array([[0, 7, -1, 3, 1],
 b = np.array([5, 7, 2, 3, 4], float)
 
 GaussElimination_complete_pivoting(A, b)
-# print(np.linalg.solve(A, b)) # for checking
+print(np.linalg.solve(A, b)) # for checking
